aaf53e51bf76aafa46729531b7e85be41a8b603a
Le Anh Tien <tienla5>
2022-11-16 13:16:30 +0700

    init project

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "assets/scss/design-system/_breakpoints")],
+    prependData: "@import \"./assets/scss/desgin-system/_breakpoints.scss\""
+  }
+};
+
+module.exports = nextConfig;
