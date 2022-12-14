d76ba94be0df65b894c5376cdd025857fd86c82b
Le Anh Tien <tienla5>
2022-12-15 06:03:37 +0700

    feat: handle call video

--- src/views/index.ts
+++ src/views/index.ts
@@ -3,4 +3,3 @@ export { default as VerifyEmailView } from "./VerifyEmailView";
 export { default as DatingView } from "./DatingView";
 export { default as CreateAccountView } from "./CreateAccountView";
 export { default as MessagesView } from "./MessagesView";
-export { default as PeerPageView } from "./PeerPageView";
